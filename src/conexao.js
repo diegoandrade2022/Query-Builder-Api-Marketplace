224c6b6532ab69eef498aa6095ebe6f16f0dfefb
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-06-21 21:49:50 -0300

    Configurando conexão do banco de dados no Keroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'market_cubos',
-        password: '8785',
+        user: 'tcvcivyxzbaqil',
+        host: 'ec2-54-157-16-196.compute-1.amazonaws.com',
+        database: 'd27tkorq20vmht',
+        password: 'e9edfa92322217059a94143c2f0885e5cc3570d24d470d048aedd5718f0a7f0c',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
